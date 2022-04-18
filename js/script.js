6937c5402a9f52be3c17d16053d6eb742185cbf3
piwang1994 <1445207429@qq.com>
2022-04-18 17:52:57 +0800

    google map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// function initialize() {
+//     var mapProp = {
+//         center: new google.maps.LatLng(114.508, 32.013),
+//         zoom: 7,
+//         zoomControl: true,
+//         mapTypeControl: true,
+//         scaleControl: true,
+//         streetViewControl: true,
+//         overviewMapControl: true,
+//         rotateControl: true,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+//     var map = new google.maps.Map(document.getElementById("map"), mapProp);
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
+
 // function initMap() {
 //     const home = { lat: 10.0452, lng: 105.7469 };
 //     const school = { lat: 16.0545, lng: 108.1717 };
@@ -64,31 +81,72 @@
 
 // window.initMap = initMap;
 
-function init() {
-    var mycenter = new google.maps.LatLng(34.183506, 112.142356);
+// Initialize and add the map
+// Initialize and add the map
+// function initMap() {
+//     // The map, centered at Uluru
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 4,
+//         center: uluru,
+//     });
 
-    var mapProp = {
-        center: mycenter,
-        zoom: 7,
-        zoomControl: true,
-        mapTypeControl: true,
-        scaleControl: true,
-        streetViewControl: true,
-        overviewMapControl: true,
-        rotateControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapProp);
-    var mycity = new google.maps.Circle({
-        center: mycenter,
-        radius: 25600,
-        strokeColor: "#FF0011",
-        strokeOpacity: 0.9,
-        strokeWeight: 2,
-        fillColor: "#000055",
-        fillOpacity: 0.4
+//     // The location of Uluru
+//     const work = { lat: 22.550491, lng: 113.967125 };
+
+//     // The marker, positioned at Uluru
+//     const marker = new google.maps.Marker({
+//         position: uluru,
+//         map: map,
+//     });
+// }
+
+// window.initMap = initMap;
+
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+function initMap() {
+    const uluru = { lat: -25.363, lng: 131.044 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
     });
-    mycity.setMap(map);
-};
+    const contentString =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+        '<div id="bodyContent">' +
+        "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+        "sandstone rock formation in the southern part of the " +
+        "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+        "south west of the nearest large town, Alice Springs; 450&#160;km " +
+        "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+        "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+        "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+        "Aboriginal people of the area. It has many springs, waterholes, " +
+        "rock caves and ancient paintings. Uluru is listed as a World " +
+        "Heritage Site.</p>" +
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+        "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+        "(last visited June 22, 2009).</p>" +
+        "</div>" +
+        "</div>";
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+    });
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map,
+        title: "Uluru (Ayers Rock)",
+    });
 
-google.maps.event.addDomListener(window, 'load', init);
+    marker.addListener("click", () => {
+        infowindow.open({
+            anchor: marker,
+            map,
+            shouldFocus: false,
+        });
+    });
+}
+
+window.initMap = initMap;
